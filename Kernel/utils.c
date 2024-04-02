a1d6ab728357632689132b3eeb7609b0d9070f14
Harel Sigron <arel1234123@gmail.com>
2024-04-02 15:45:38 +0300

    Removed useless functions

--- Kernel/utils.c
+++ Kernel/utils.c
@@ -9,15 +9,6 @@ void* memset(void* addr, uint8_t c, size_t n) {
   return addr;
 }
 
-void* memsetw(void* addr, uint16_t val, size_t n) {
-  
-  for (uint32_t i = 0; i < n / 2; i++) {
-    ((uint16_t*)addr)[i] = val;
-  }
-
-  return addr;
-}
-
 void memcpy(void* s1, const void* s2, size_t n) {
   for (; n > 0; n--) { *(uint8_t*)s1++ = *(uint8_t*)s2++; }
 }
@@ -33,21 +24,6 @@ void* memmove(void* dstptr, const void* srcptr, size_t size) {
 			dst[i-1] = src[i-1];
 	}
 	return dstptr;
-}
-
-/* Check if a character is in the English alphabet */
-bool isalpha(char c) {  
-  return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) ? true : false;
-}
-
-/* Check if a character is a uppercase character */
-bool isupper(char c) {
-  return (c >= 'A' && c <= 'Z') ? true : false;
-}
-
-/* Check if a character is a lowercase character */
-bool islower(char c) {
-  return (c >= 'a' && c <= 'z') ? true : false;
 }
 
 /*
@@ -141,71 +117,6 @@ size_t strl(const char* str) {
   return len;
 }
 
-/* Get the digit length of an integer */
-size_t intl(int n) {
-  
-  size_t length = 0;
-  while (n > 0) { length++; n /= 10; }
-
-  return length;
-}
-/*
- This function converts the given size_t value to its string representation
- using the specified base. The result is stored in the provided character buffer.
-*/
-char* itoa_size_t(size_t value, char* buffer, size_t size, size_t base) 
-{
-    if (size == 0) {
-        // Invalid buffer size
-        return NULL;
-    }
-
-    size_t i = 0;
-
-    // Special case for 0
-    if (value == 0) {
-        buffer[i++] = '0';
-    } else {
-        // Convert the value to a string in reverse order
-        while (value > 0 && i < size - 1) {
-            size_t digit = value % base;
-            buffer[i++] = (digit < 10) ? ('0' + digit) : ('a' + digit - 10);
-            value /= base;
-        }
-    }
-
-    // Reverse the string
-    for (size_t j = 0; j < i / 2; j++) {
-        char temp = buffer[j];
-        buffer[j] = buffer[i - j - 1];
-        buffer[i - j - 1] = temp;
-    }
-
-    // Null-terminate the string
-    buffer[i] = '\0';
-
-    return buffer;
-}
-/*
-itoa turns an integer into a char array
-Input: Integer to convert, base to represent as (e.g 10 for decimal, 16 for hexadecimal..)
-*/
-char* itoa(int value, size_t base) {
-
-  static char buf[32] = { 0 };
-
-  if (!value) { return "0"; }     // If value is 0, return a 0 string
-  if (base > 16) { return NULL; } // Can't convert bases above hexadecimal
-
-  size_t i = 30;
-
-  for (; value && i; i--, value /= base) {
-    buf[i] = "0123456789abcdef"[value % base];
-  }
-  
-  return &buf[i+1];
-}
-
 /* Copy contents of one string into another */
 char* strcpy(char* dest, const char* src) { 
   
@@ -243,33 +154,7 @@ char* strncat(char* dest, const char* src, size_t n) {
   return dest;
 }
 
-/* Make all lower characters in the string uppercase */
-char* upper(char* src, size_t n) {
-  
-  for (unsigned long i = 0; i < n; i++) { 
-    if (islower(src[i])) { src[i] += ('A' - 'a'); }
-  }
-
-  return src;
-}
-
 int strcmp(const char* s1, const char* s2) {
   while(*s1 && (*s1 == *s2)) { s1++; s2++; }
   return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
-
-void strrev(char* str) {
-
-  size_t size = strl(str);
-  char c = 0;
-  
-  for (unsigned long i = 0; i < size / 2; i++) {
-    c = str[i];
-    str[i] = str[size - i - 1];
-    str[size - i - 1] = c;
-  }
-}
-
-
-
-
